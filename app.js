c9957352b735676f46dd1ef34e259f00ee2a435b
Muhaamd Jamshed <eyes0033@gmail.com>
2021-11-15 00:26:12 +0500

    first comment

---
+++ app.js
@@ -0,0 +1,138 @@
+// alert("jamshed")
+
+
+// Q=
+
+// var date =new Date()
+// document.write(date)
+
+
+// Q==
+// var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// var date =new Date()
+// var month = months[date.getMonth()]
+// // date.getMonth()
+// document.write(month)
+
+
+// Q=======
+
+// const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var date =new Date()
+// var month = days[date.getDay()]
+// // date.getMonth()
+// document.write(month)
+
+
+// Q==========
+// var mo = prompt("enter days").toUpperCase
+// const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var date = new Date()
+// var da=days[date.getDay()]
+// if(date=="SUNDAY" || date=="SATURDAY"){
+//     document.write("Its Fun Day")
+// }
+// else{
+//     document.write("Its working day")
+// }
+
+// Q==
+
+// var date = new Date();
+// var firstDay = new Date(date.getFullYear(), date.getMonth(), 15);
+// document.write(firstDay)
+
+
+// Q===
+
+// var date = new Date();
+// document.write(date +"<br>");
+
+// var da = new Date();
+// var ms = da.setMilliseconds( 1,0,1970 );
+// document.write(ms +"<br>")
+
+
+// var da = new Date();
+// var ms = da.setMinutes( 1,0,1970);
+// document.write(ms +"<br>")
+
+
+// Q======
+
+// var a = new Date();
+// var b = a.toString();
+
+// var c = b.slice(16,19);
+// // document.write(c)
+// if (c === "12:"){
+//     document.write("It's PM")
+// }else if(c === "13:"){
+//     document.write("It's PM")
+// }else if(c === "14:"){
+//     document.write("It's PM")
+// }else if(c === "15:"){
+//     document.write("It's PM")
+// }else if(c === "16:"){
+//     document.write("It's PM")
+// }else if(c === "17:"){
+//     document.write("It's PM")
+// }else if(c === "18:"){
+//     document.write("It's PM")
+// }else if(c === "19:"){
+//     document.write("It's PM")
+// }else if(c === "20:"){
+//     document.write("It's PM")
+// }else if(c === "21:"){
+//     document.write("It's PM")
+// }else if(c === "22:"){
+//     document.write("It's PM")
+// }else if(c === "23:"){
+//     document.write("It's PM")
+// }else if(c === "24:"){
+//     document.write("It's AM")
+// }else if(c === "01:"){
+//     document.write("It's AM")
+// }else if(c === "02:"){
+//     document.write("It's AM")
+// }else if(c === "03:"){
+//     document.write("It's AM")
+// }else if(c === "04:"){
+//     document.write("It's AM")
+// }else if(c === "05:"){
+//     document.write("It's AM")
+// }else if(c === "06:"){
+//     document.write("It's AM")
+// }else if(c === "07:"){
+//     document.write("It's AM")
+// }else if(c === "08:"){
+//     document.write("It's AM")
+// }else if(c === "09:"){
+//     document.write("It's AM")
+// }else if(c === "10:"){
+//     document.write("It's AM")
+// }else if(c === "11:"){
+//     document.write("It's AM")
+// }
+
+
+
+// Q======
+// var date =new Date()
+// var newdate = date.laterdate(2020)
+// document.write(newdate)
+
+
+// var names =new Date()
+// names.setFullYear(2019,31,4)
+// document.write(names)
+
+
+
+
+
+
+
+
+
+
